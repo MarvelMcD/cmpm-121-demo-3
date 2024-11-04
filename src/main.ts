c6f497be43eec29430383a01571257ff9dbc6f1f
MarvelMcD <155485365+MarvelMcD@users.noreply.github.com>
2024-11-04 10:21:26 -0800

    button test

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
-// todo
+const app: HTMLDivElement = document.querySelector("#app")!;
+
+const alertButton = document.createElement("button");
+alertButton.textContent = "Click Me!";
+
+alertButton.addEventListener("click", () => {
+  alert("You clicked the button!");
+});
+
+app.append(alertButton);
