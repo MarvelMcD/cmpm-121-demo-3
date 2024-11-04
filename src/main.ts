e0533e5cbcd5d5aebbebf93c1d56ca470ccbbd2a
MarvelMcD <155485365+MarvelMcD@users.noreply.github.com>
2024-11-04 10:25:00 -0800

    test

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 
 const alertButton = document.createElement("button");
 alertButton.textContent = "Click Me!";
-
+// test
 alertButton.addEventListener("click", () => {
   alert("You clicked the button!");
 });
